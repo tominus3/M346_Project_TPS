d1336a2401adcb69f058466fe6fc54646e7f8a81
tominus3 <tomnielsen2211@gmail.com>
2025-12-10 11:31:57 +0100

    Added example code from amazon and made some changes

--- LambdaFunction/CelebrityRecogniser/src/CelebrityRecogniser/Function.cs
+++ LambdaFunction/CelebrityRecogniser/src/CelebrityRecogniser/Function.cs
@@ -1,21 +1,59 @@
 using Amazon.Lambda.Core;
+using System.Threading.Tasks;
+using Amazon.Rekognition;
+using Amazon.Rekognition.Model;
+using System;
+using System.IO;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
 
-namespace CelebrityRecogniser;
 
-public class Function
+public class CelebritiesInImage
 {
-    
-    /// <summary>
-    /// A simple function that takes a string and does a ToUpper
-    /// </summary>
-    /// <param name="input">The event for the Lambda function handler to process.</param>
-    /// <param name="context">The ILambdaContext that provides methods for logging and describing the Lambda environment.</param>
-    /// <returns></returns>
-    public string FunctionHandler(string input, ILambdaContext context)
+    public async static Task Example()
     {
-        return input.ToUpper();
+        string photo = Path.Combine(AppContext.BaseDirectory, "bean.jpg");
+
+        AmazonRekognitionClient rekognitionClient = new AmazonRekognitionClient();
+
+        RecognizeCelebritiesRequest recognizeCelebritiesRequest = new RecognizeCelebritiesRequest();
+
+        Amazon.Rekognition.Model.Image img = new Amazon.Rekognition.Model.Image();
+        byte[] data = null;
+        try
+        {
+            using (FileStream fs = new FileStream(photo, FileMode.Open, FileAccess.Read))
+            {
+                data = new byte[fs.Length];
+                fs.Read(data, 0, (int)fs.Length);
+            }
+        }
+        catch(Exception)
+        {
+            Console.WriteLine("Failed to load file " + photo);
+            return;
+        }
+
+        img.Bytes = new MemoryStream(data);
+        recognizeCelebritiesRequest.Image = img;
+
+        Console.WriteLine("Looking for celebrities in image " + photo + "\n");
+
+        RecognizeCelebritiesResponse recognizeCelebritiesResponse = await rekognitionClient.RecognizeCelebritiesAsync(recognizeCelebritiesRequest);
+
+        Console.WriteLine(recognizeCelebritiesResponse.CelebrityFaces.Count + " celebrity(s) were recognized.\n");
+        foreach (Celebrity celebrity in recognizeCelebritiesResponse.CelebrityFaces)
+        {
+            Console.WriteLine("Celebrity recognized: " + celebrity.Name);
+            Console.WriteLine("Celebrity ID: " + celebrity.Id);
+            BoundingBox boundingBox = celebrity.Face.BoundingBox;
+            Console.WriteLine("position: " +
+               boundingBox.Left + " " + boundingBox.Top);
+            Console.WriteLine("Further information (if available):");
+            foreach (String url in celebrity.Urls)
+                Console.WriteLine(url);
+        }
+        Console.WriteLine(recognizeCelebritiesResponse.UnrecognizedFaces.Count + " face(s) were unrecognized.");
     }
 }

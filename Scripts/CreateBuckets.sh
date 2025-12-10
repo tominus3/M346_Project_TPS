18a3af55441d1af0ef1917ce07bf86cc58464fd3
PauloCapelos <PauloCapelos@edu.gbssg.ch>
2025-12-10 11:07:37 +0100

    Implement code for creating input S3 bucket and configuring it

--- Scripts/CreateBuckets.sh
+++ Scripts/CreateBuckets.sh
@@ -4,3 +4,14 @@
 # Description:	Erstellt zwei konfigurierte Buckets
 # Author:	Paulo Capelos
 # Date:		03.12.2025
+
+BUCKET_NAME=input-bucket-m346-project
+
+aws s3 mb s3://$BUCKET_NAME
+
+function configure_bucket()
+{
+	aws s3api put-public-access-block --bucket $BUCKET_NAME --public-access-block-configuration "BlockPublicPolicy=false"
+}
+
+configure_bucket

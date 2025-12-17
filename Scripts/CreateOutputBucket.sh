c7341162787f5e88cad5cac678e86621d532afbc
PauloCapelos <vianapaulo576@gmail.com>
2025-12-17 08:56:12 +0100

    Renamed the script to CreateOutputBucket.sh

---
+++ Scripts/CreateOutputBucket.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Script:	CreateBuckets.sh
+# Description:	Erstellt zwei konfigurierte Buckets
+# Author:	Paulo Capelos
+# Date:		03.12.2025
+
+BUCKET_NAME=output-bucket-m346-project
+
+aws s3 mb s3://$BUCKET_NAME
+
+echo "Bucket created"
+
+function configure_bucket()
+{
+	aws s3api put-public-access-block --bucket $BUCKET_NAME --public-access-block-configuration "BlockPublicPolicy=false"
+}
+
+function activate_ACL()
+{
+	aws s3api put-bucket-ownership-controls --bucket $BUCKET_NAME --ownership-controls="Rules=[{ObjectOwnership=BucketOwnerPreferred}]"
+}
+
+configure_bucket
+
+echo "Bucket configured"
+
+activate_ACL
+
+echo "Creation output bucket completed"

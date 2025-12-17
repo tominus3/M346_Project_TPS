8251aee357514172cff778cec08c84ff15e14678
PauloCapelos <vianapaulo576@gmail.com>
2025-12-17 08:47:47 +0100

    Implement the ACL activation and add echo's

--- Scripts/CreateBuckets.sh
+++ Scripts/CreateBuckets.sh
@@ -9,9 +9,22 @@ BUCKET_NAME=input-bucket-m346-project
 
 aws s3 mb s3://$BUCKET_NAME
 
+echo "Bucket created"
+
 function configure_bucket()
 {
 	aws s3api put-public-access-block --bucket $BUCKET_NAME --public-access-block-configuration "BlockPublicPolicy=false"
 }
 
+function activate_ACL()
+{
+	aws s3api put-bucket-ownership-controls --bucket $BUCKET_NAME --ownership-controls="Rules=[{ObjectOwnership=BucketOwnerPreferred}]"
+}
+
 configure_bucket
+
+echo "Bucket configured"
+
+activate_ACL
+
+echo "Creation completed"

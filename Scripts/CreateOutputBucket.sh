89043fe892d0623a54d88cbf3c2a7f3610cb8283
PauloCapelos <vianapaulo576@gmail.com>
2025-12-21 12:54:59 +0100

    Implement the possibility to choose the bucket name

--- Scripts/CreateOutputBucket.sh
+++ Scripts/CreateOutputBucket.sh
@@ -5,27 +5,26 @@
 # Author:	Paulo Capelos
 # Date:		17.12.2025
 
-BUCKET_NAME="output-bucket-m346-project"
 REGION="us-east-1"
 
 if aws s3api head-bucket --bucket "$BUCKET_NAME" 2>/dev/null; then
-    echo "Bucket $BUCKET_NAME Bucket already exists"
+    echo "Bucket $OUTPUT_BUCKET_NAME Bucket already exists"
 else
     echo "Create bucket"
-    aws s3 mb s3://$BUCKET_NAME --region $REGION
+    aws s3 mb s3://$OUTPUT_BUCKET_NAME --region $REGION
 fi
 
 function configure_bucket()
 {
 	aws s3api put-public-access-block \
-	--bucket $BUCKET_NAME \
+	--bucket $OUTPUT_BUCKET_NAME \
 	--public-access-block-configuration "BlockPublicPolicy=false"
 }
 
 function activate_ACL()
 {
 	aws s3api put-bucket-ownership-controls \
-	--bucket $BUCKET_NAME \
+	--bucket $OUTPUT_BUCKET_NAME \
 	--ownership-controls="Rules=[{ObjectOwnership=BucketOwnerPreferred}]"
 }
 
--- Scripts/init.sh
+++ Scripts/init.sh
@@ -5,11 +5,32 @@
 # Author:	Paulo Capelos
 # Date:		21.12.2025
   
+ echo "Bitte gib eine Bezeichnung, welche am ende des Buckets hinzugefügt wird"
+ echo "Beispiel: output-bucket-m346-project-jeff" 
+
+read INPUT
+
+if [ -z "$INPUT" ]; then
+    echo "Kein Name eingegeben. Abbruch."
+    exit 1
+fi
+
+UNIQUE_BUCKET_NAME=${INPUT,,}
+  
+export INPUT_BUCKET_NAME="input-bucket-m346-project-${UNIQUE_BUCKET_NAME}"
+export OUTPUT_BUCKET_NAME="output-bucket-m346-project-${UNIQUE_BUCKET_NAME}"  
+  
 ./CreateInputBucket.sh
+
+echo "-------------------------------------"
  
 ./CreateOutputBucket.sh
- 
+
+echo "-------------------------------------"
+
 ./CreateLambdaFunction.sh
+
+echo "-------------------------------------"
  
 ./CreateS3Trigger.sh
 

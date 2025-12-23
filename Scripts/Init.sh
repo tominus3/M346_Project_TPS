fe57b4fd2cc7705663ee161065e8e489d57275f2
PauloCapelos <vianapaulo576@gmail.com>
2025-12-23 12:31:34 +0100

    Add comments to the Init.sh file

--- Scripts/Init.sh
+++ Scripts/Init.sh
@@ -4,28 +4,33 @@
 # Description:	Führt alle Skripts aus
 # Author:	Paulo Capelos
 # Date:		21.12.2025
+# Source: 	-
 
 export AWS_DEFAULT_REGION="us-east-1"
-  
+
+# 1. Benutzerinteraktion
  echo "Bitte gib eine Bezeichnung, welche am ende des Buckets hinzugefügt wird"
  echo "Beispiel: output-bucket-m346-project-[Ihre Eingabe]" 
 
 read INPUT
 
+# 2. Eingabeprüfung
 if [ -z "$INPUT" ]; then
     echo "Kein Name eingegeben. Abbruch."
     exit 1
 fi
 
+# 3. Datenaufbereitung
 UNIQUE_BUCKET_NAME=${INPUT,,}
   
 INPUT_BUCKET_NAME="input-bucket-m346-project-${UNIQUE_BUCKET_NAME}"
 OUTPUT_BUCKET_NAME="output-bucket-m346-project-${UNIQUE_BUCKET_NAME}"  
 
-# Wir schreiben die echten Namen in eine Datei
+# 4. Die echten Namen in eine Datei schreiben
 echo "INPUT_BUCKET_NAME=$INPUT_BUCKET_NAME" > BucketNames
 echo "OUTPUT_BUCKET_NAME=$OUTPUT_BUCKET_NAME" >> BucketNames
-  
+ 
+# 5. Schritt für Schritt Aufruf
 echo "-------------------------------------"
   
 ./Additional/CreateInputBucket.sh
